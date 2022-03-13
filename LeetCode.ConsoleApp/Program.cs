0fcf7e6be68525cd0591b1ab9d29227ffa8871d0
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-03-13 17:23:24 +0200

    Create project structure

---
+++ LeetCode.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
