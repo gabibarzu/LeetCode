306a333418eff872121874a3914e1cfcab9be9ec
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-03-29 20:51:29 +0300

    238. Product of Array Except Self

---
+++ LeetCode.Solutions/ArraysAndStrings/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,85 @@
+﻿namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// 238. Product of Array Except Self
+    /// URL: https://leetcode.com/problems/product-of-array-except-self/
+    /// </summary>
+    public class ProductOfArrayExceptSelf
+    {
+        /// <summary>
+        /// Time complexity: O(n)
+        /// Space complexity: O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var leftProduct = new int[nums.Length];
+            var rightProduct = new int[nums.Length];
+            var result = new int[nums.Length];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    leftProduct[i] = 1;
+                }
+                else
+                {
+                    leftProduct[i] = nums[i - 1] * leftProduct[i - 1];
+                }
+            }
+
+            for (var j = nums.Length - 1; j >= 0; j--)
+            {
+                if (j == nums.Length - 1)
+                {
+                    rightProduct[j] = 1;
+                }
+                else
+                {
+                    rightProduct[j] = nums[j + 1] * rightProduct[j + 1];
+                }
+            }
+
+            for (var k = 0; k < nums.Length; k++)
+            {
+                result[k] = leftProduct[k] * rightProduct[k];
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] ProductExceptSelfOptimized(int[] nums)
+        {
+            var result = new int[nums.Length];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    result[i] = 1;
+                }
+                else
+                {
+                    result[i] = nums[i - 1] * result[i - 1];
+                }
+            }
+
+            var right = 1;
+            for (var j = nums.Length - 1; j >= 0; j--)
+            {
+                result[j] = right * result[j];
+                right *= nums[j];   
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysAndStrings/ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysAndStrings
+{
+    [TestClass]
+    public class ProductOfArrayExceptSelfTests
+    {
+
+        [TestMethod]
+        public void Test_1()
+        {
+            var nums = new[] { 1, 2, 3, 4 };
+            var solution = ProductOfArrayExceptSelf.ProductExceptSelf(nums);
+            solution.Should().Contain(24).And.Contain(12).And.Contain(8).And.Contain(6);
+        }
+    }
+}
