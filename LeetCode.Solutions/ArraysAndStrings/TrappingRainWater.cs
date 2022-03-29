05d80fb7c104b0ebf3fcb785bef705a1decd87ae
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-03-29 17:40:59 +0300

    42. Trapping Rain Water

---
+++ LeetCode.Solutions/ArraysAndStrings/TrappingRainWater.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// 42. Trapping Rain Water
+    /// URL: https://leetcode.com/problems/trapping-rain-water
+    /// Time complexity: O(n)
+    /// Space complexity: O(1)
+    /// </summary>
+    public class TrappingRainWater
+    {
+        public static int Trap(int[] height)
+        {
+            var left = 0;
+            var right = height.Length - 1;
+            var maxLeft = height[left];
+            var maxRight = height[right];
+            var water = 0;
+
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    if (height[left] >= maxLeft)
+                    {
+                        maxLeft = height[left];
+                    }
+                    else
+                    {
+                        water += (maxLeft - height[left]);
+                    }
+                    left++;
+                }
+                else
+                {
+                    if (height[right] >= maxRight)
+                    {
+                        maxRight = height[right];
+                    }
+                    else
+                    {
+                        water += maxRight - height[right];
+                    }
+                    right--;
+                }
+            }
+
+            return water;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysAndStrings/TrappingRainWaterTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysAndStrings
+{
+    [TestClass]
+
+    public class TrappingRainWaterTests
+    {
+
+        [TestMethod]
+        public void Test_1()
+        {
+            var height = new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            var solution = TrappingRainWater.Trap(height);
+            solution.Should().Be(6);
+        }
+    }
+}
