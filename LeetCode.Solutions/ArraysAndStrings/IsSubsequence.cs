397a2316edb778db3f89808bf5ae2143228029a9
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-03-31 21:43:22 +0300

    392. Is Subsequence

---
+++ LeetCode.Solutions/ArraysAndStrings/IsSubsequence.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// 392. Is Subsequence
+    /// URL: https://leetcode.com/problems/is-subsequence/
+    /// Time complexity: O(t)
+    /// Space complexity: O(1)
+    /// </summary>
+    public class IsSubsequence
+    {
+        public static bool IsSubsequenceSolution(string s, string t)
+        {
+            var sSize = s.Length;
+            var tSize = t.Length;
+            var i = 0;
+            var j = 0;
+
+            while (i < sSize && j < tSize)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+                j++;
+            }
+
+            return i == sSize;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysAndStrings/IsSubsequenceTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.ArraysAndStrings;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysAndStrings
+{
+    [TestClass]
+    public class IsSubsequenceTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var s = "abc";
+            var t = "ahbgdc";
+            var solution = IsSubsequence.IsSubsequenceSolution(s, t);
+            solution.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var s = "axc";
+            var t = "ahbgdc";
+            var solution = IsSubsequence.IsSubsequenceSolution(s, t);
+            solution.Should().BeFalse();
+        }
+    }
+}
