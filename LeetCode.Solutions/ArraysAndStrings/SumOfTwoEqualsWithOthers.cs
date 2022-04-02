778768284f47583bed70fbb1ca5faaa7a1e3afd3
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-02 19:42:24 +0300

    SumOfTwoEqualsWithOthers

---
+++ LeetCode.Solutions/ArraysAndStrings/SumOfTwoEqualsWithOthers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// Check if there exist two elements in an array whose sum is equal to the sum of rest of the array
+    /// URL: https://www.geeksforgeeks.org/check-exist-two-elements-array-whose-sum-equal-sum-rest-array/
+    /// Time complexity: O(n)
+    /// Space complexity: O(n)
+    /// </summary>
+    public class SumOfTwoEqualsWithOthers
+    {
+        public static bool Solution(int[] array)
+        {
+            if (!array.Any())
+            {
+                return false;
+            }
+
+            var sum = array.Sum();
+            if (sum % 2 == 1)
+            {
+                return false;
+            }
+
+            var sumOfTwo = sum / 2;
+            var values = new Dictionary<int, int>();
+            foreach (var item in array)
+            {
+                var needed = sumOfTwo - item;
+                if (values.ContainsKey(needed))
+                {
+                    return true;
+                }
+                else
+                {
+                    values.Add(item, needed);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysAndStrings/SumOfTwoEqualsWithOthersTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysAndStrings
+{
+    [TestClass]
+    public class SumOfTwoEqualsWithOthersTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var array = new[] { 2, 11, 5, 1, 4, 7 };
+            var solution = SumOfTwoEqualsWithOthers.Solution(array);
+            solution.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var array = new[] { 2, 4, 2, 1, 11, 15 };
+            var solution = SumOfTwoEqualsWithOthers.Solution(array);
+            solution.Should().BeFalse();
+        }
+    }
+}
