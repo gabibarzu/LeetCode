883f9523aca83d0cb3f306dfb9237ad457f7baa1
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-02 20:03:10 +0300

    252. Meeting Rooms

---
+++ LeetCode.Solutions/ArraysAndStrings/MeetingRooms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// 252. Meeting Rooms
+    /// URL: https://leetcode.com/problems/meeting-rooms/
+    /// Time complexity: O (n log n)
+    /// Time complexity: O (1)
+    /// </summary>
+    public class MeetingRooms
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            for (var i = 0; i < intervals.Length - 1; i++)
+            {
+                if (intervals[i][1] > intervals[i + 1][0])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
