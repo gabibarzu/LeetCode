84480c580fafa284169466fee68022e6546ddce1
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-03 20:57:18 +0300

    322. Coin Change

---
+++ LeetCode.Solutions/DynamicProgramming/CoinChange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Solutions.DynamicProgramming
+{
+    /// <summary>
+    /// 322. Coin Change
+    /// Url: https://leetcode.com/problems/coin-change/
+    /// Time complexity: O (S * N) S - amount, N - coins
+    /// Time complexity: O (N) N - coins
+    /// </summary>
+    public class CoinChange
+    {
+        public static int CoinChangeSolution(int[] coins, int amount)
+        {
+            var dp = new int[amount + 1];
+            Array.Fill(dp, amount + 1);
+            dp[0] = 0;
+            for (var i = 1; i <= amount; i++)
+            {
+                foreach (var coin in coins)
+                {
+                    if (coin <= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/CoinChangeTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.DynamicProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.DynamicProgramming
+{
+    [TestClass]
+    public class CoinChangeTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var coins = new[] { 1, 2, 5 };
+            const int amount = 11;
+            var result = CoinChange.CoinChangeSolution(coins, amount);
+
+            result.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var coins = new[] { 186, 419, 83, 408 };
+            const int amount = 6249;
+            var result = CoinChange.CoinChangeSolution(coins, amount);
+
+            result.Should().Be(20);
+        }
+    }
+}
