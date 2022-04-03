c2a9913973c2313bacd396b08a63fce186cc15a4
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-03 09:29:20 +0300

    9. Palindrome Number

---
+++ LeetCode.Solutions/ArraysAndStrings/PalindromeNumber.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Solutions.ArraysAndStrings
+{
+    /// <summary>
+    /// 9. Palindrome Number
+    /// Url: https://leetcode.com/problems/palindrome-number/
+    /// Time complexity: O(log 10 n)
+    /// Space complexity: O(1)
+    /// </summary>
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || x % 10 == 0 && x != 0)
+            {
+                return false;
+            }
+
+            var revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }        
+        
+        public bool IsPalindrome_2(int x)
+        {
+            var tempValue = x;
+            var reverse = 0;
+            while (tempValue > 0)
+            {
+                reverse = reverse * 10 + tempValue % 10;
+                tempValue /= 10;
+            }
+
+            return reverse == x;
+        }
+    }
+}
