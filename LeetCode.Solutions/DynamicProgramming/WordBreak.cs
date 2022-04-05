788d3b20c7558eb6f1dfd14e54130d3524370c5b
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-05 22:56:02 +0300

    139. Word Break

---
+++ LeetCode.Solutions/DynamicProgramming/WordBreak.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.DynamicProgramming
+{
+    /// <summary>
+    /// 139. Word Break
+    /// Url: https://leetcode.com/problems/word-break/
+    /// </summary>
+    public class WordBreak
+    {
+        /// <summary>
+        /// Recursive with memoization
+        /// Time complexity : O(n^3)
+        /// Space complexity : O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="wordDict"></param>
+        /// <returns></returns>
+        private static readonly Dictionary<string, bool> Cache = new();
+
+        public static bool WordBreakRecursive(string s, IList<string> wordDict)
+        {
+            if (wordDict.Contains(s))
+            {
+                return true;
+            }
+            if (Cache.ContainsKey(s))
+            {
+                return Cache[s];
+            }
+            for (var i = 1; i < s.Length; i++)
+            {
+                var left = s.Substring(0, i);
+                if (wordDict.Contains(left) && WordBreakRecursive(s.Substring(i), wordDict))
+                {
+                    Cache.Add(s, true);
+                    return true;
+                }
+            }
+            Cache.Add(s, false);
+            return false;
+        }
+
+        /// <summary>
+        /// Dynamic Programming
+        /// Time complexity : O(n^3)
+        /// Space complexity : O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="wordDict"></param>
+        /// <returns></returns>
+        public static bool WordBreakDynamicProgramming(string s, IList<string> wordDict)
+        {
+            var wordDictSet = new SortedSet<string>(wordDict);
+            var dp = new bool[s.Length + 1];
+            dp[0] = true;
+            for (var i = 1; i <= s.Length; i++)
+            {
+                for (var j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordDictSet.Contains(s.Substring(j, i)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.Length];
+        }
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/WordBreakTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using LeetCode.Solutions.DynamicProgramming;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace LeetCode.Tests.DynamicProgramming
+{
+    [TestClass]
+    public class WordBreakTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var s = "leetcode";
+            var wordDict = new List<string> { "leet", "code" };
+
+            var result = WordBreak.WordBreakRecursive(s, wordDict);
+
+            result.Should().BeTrue();
+        }
+    }
+}
