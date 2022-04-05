442a2d077e9835c30c3626aa65ef8bae55fc64fe
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-05 23:28:35 +0300

    53. Maximum Subarray

---
+++ LeetCode.Solutions/DynamicProgramming/MaximumSubarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Solutions.DynamicProgramming
+{
+    /// <summary>
+    /// 53. Maximum Subarray
+    /// Url: https://leetcode.com/problems/maximum-subarray/
+    /// Time complexity: O(n)
+    /// Space complexity: O(1)
+    /// </summary>
+    public class MaximumSubArray
+    {
+        public int maxSubArray(int[] nums)
+        {
+            // Initialize our variables using the first element.
+            var currentSubArray = nums[0];
+            var maxSubArray = nums[0];
+
+            // Start with the 2nd element since we already used the first one.
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+                currentSubArray = Math.Max(num, currentSubArray + num);
+                maxSubArray = Math.Max(maxSubArray, currentSubArray);
+            }
+
+            return maxSubArray;
+        }
+    }
+}
