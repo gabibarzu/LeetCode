3f472e3018d76f1ae03ee89e8a5fe8e0b32b40a1
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-06 23:48:03 +0300

    416. Partition Equal Subset Sum

---
+++ LeetCode.Solutions/DynamicProgramming/PartitionEqualSubsetSum.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCode.Solutions.DynamicProgramming
+{
+    /// <summary>
+    /// 416. Partition Equal Subset Sum
+    /// Url: https://leetcode.com/problems/partition-equal-subset-sum/
+    /// </summary>
+    public class PartitionEqualSubsetSum
+    {
+        public static bool CanPartition(int[] nums)
+        {
+            var sum = nums.Sum();
+
+            if (sum % 2 == 1)
+            {
+                return false;
+            }
+
+            if (nums.Length == 2)
+            {
+                return nums[0] == nums[1];
+            }
+
+            var half = sum / 2;
+            var halfResult = 0;
+            foreach (var num in nums)
+            {
+                if (num < half && halfResult + num <= half)
+                {
+                    halfResult += num;
+                }
+
+                if (halfResult == half)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/DynamicProgramming/PartitionEqualSubsetSumTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.DynamicProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.DynamicProgramming
+{
+    [TestClass]
+    public class PartitionEqualSubsetSumTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var nums = new int[] { 1, 5, 11, 5 };
+
+            var result = PartitionEqualSubsetSum.CanPartition(nums);
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var nums = new int[] { 1, 1 };
+
+            var result = PartitionEqualSubsetSum.CanPartition(nums);
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Test_3()
+        {
+            var nums = new int[] { 2, 2, 1, 1 };
+
+            var result = PartitionEqualSubsetSum.CanPartition(nums);
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void Test_4()
+        {
+            var nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var result = PartitionEqualSubsetSum.CanPartition(nums);
+
+            result.Should().BeTrue();
+        }
+    }
+}
