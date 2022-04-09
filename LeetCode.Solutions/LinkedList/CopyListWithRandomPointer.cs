f2256a26d4456a1b2327847f5ec4a9417c60e8ce
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-04-09 20:25:02 +0300

    138. Copy List with Random Pointer

---
+++ LeetCode.Solutions/LinkedList/CopyListWithRandomPointer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.LinkedList
+{
+    /// <summary>
+    /// 138. Copy List with Random Pointer
+    /// Url: https://leetcode.com/problems/copy-list-with-random-pointer/
+    /// Time complexity: O(n)
+    /// Space complexity: O(n)
+    /// </summary>
+    public class CopyListWithRandomPointer
+    {
+        public class Node
+        {
+            public int val;
+            public Node next;
+            public Node random;
+
+            public Node(int _val)
+            {
+                val = _val;
+                next = null;
+                random = null;
+            }
+        }
+
+        public static Node CopyRandomList(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var copies = new Dictionary<Node, Node>();
+
+            var current = head;
+            while (current != null)
+            {
+                copies.Add(current, new Node(current.val));
+                current = current.next;
+            }
+
+            current = head;
+            while (current != null)
+            {
+                var copyNode = copies.GetValueOrDefault(current);
+                if (copyNode != null)
+                {
+                    copyNode.next = current.next != null ? copies.GetValueOrDefault(current.next) : null;
+                    copyNode.random = current.random != null ? copies.GetValueOrDefault(current.random) : null;
+                }
+
+                current = current.next;
+            }
+
+            return copies[head];
+        }
+    }
+}
---
+++ LeetCode.Tests/LinkedList/CopyListWithRandomPointerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.LinkedList
+{
+    [TestClass]
+    public class CopyListWithRandomPointerTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var node1 = new CopyListWithRandomPointer.Node(7);
+            var node2 = new CopyListWithRandomPointer.Node(13);
+            var node3 = new CopyListWithRandomPointer.Node(11);
+            var node4 = new CopyListWithRandomPointer.Node(10);
+            var node5 = new CopyListWithRandomPointer.Node(1);
+
+            node1.next = node2;
+            node1.random = null;
+
+            node2.next = node3;
+            node2.random = node1;
+
+            node3.next = node4;
+            node3.random = node5;
+
+            node4.next = node5;
+            node4.random = node3;
+
+            node5.next = null;
+            node5.random = node1;
+
+            var result = CopyListWithRandomPointer.CopyRandomList(node1);
+
+            result.Should().Be(node1);
+        }
+    }
+}
